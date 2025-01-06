8538c514f43a6f8dcfbd6e0a54ce26989dda8144
Fahad TT <ttfahad2317@gmail.com>
2025-01-06 10:36:59 +0530

    style: horizontal gapping between chip widgets adjusted

--- lib/view/widgets/home widgets/chip_widget.dart
+++ lib/view/widgets/home widgets/chip_widget.dart
@@ -8,7 +8,7 @@ class ChipWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 28),
+      padding: const EdgeInsets.symmetric(horizontal: 26),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
