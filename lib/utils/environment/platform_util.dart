79b05316e9c432083a5c4aeda9ff28d1679b1fc5
Fahad TT <ttfahad2317@gmail.com>
2025-02-06 15:45:52 +0400

    fix: pasword field error fixed and feat: working on the logic for redirecting the users to the app's login page after successful mail verification

---
+++ lib/utils/environment/platform_util.dart
@@ -0,0 +1,30 @@
+import 'package:alletre_app/utils/routes/named_routes.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PlatformUtil {
+  static bool get isWebPlatform => kIsWeb;
+  
+  static String getLoginUrl(bool requiresVerification) {
+    if (isWebPlatform) {
+      // Web login URL
+      return 'https://www.alletre.com/login';
+    } else {
+      // App login route
+      return AppRoutes.login;
+    }
+  }
+  
+  static void handleLoginRedirect(BuildContext context, bool requiresVerification) {
+    final String loginDestination = getLoginUrl(requiresVerification);
+    
+    if (isWebPlatform) {
+      // For web, launch the URL
+      launchUrl(Uri.parse(loginDestination));
+    } else {
+      // For app, use Navigator
+      Navigator.pushReplacementNamed(context, loginDestination);
+    }
+  }
+}
