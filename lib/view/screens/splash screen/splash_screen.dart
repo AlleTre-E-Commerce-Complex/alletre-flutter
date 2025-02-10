088daa61580898e9a23c222f492e67280617dda0
Fahad TT <ttfahad2317@gmail.com>
2025-02-10 17:16:21 +0400

    fix: deactivated widget error solved

--- lib/view/screens/splash screen/splash_screen.dart
+++ lib/view/screens/splash screen/splash_screen.dart
@@ -5,25 +5,98 @@ import 'package:alletre_app/utils/routes/main_stack.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
-class SplashScreen extends StatelessWidget {
+
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
+
+//   void _navigateToOnBoarding(BuildContext context) {
+//    WidgetsBinding.instance.addPostFrameCallback((_) {
+//      Future.delayed(const Duration(seconds: 2), () {
+//        Navigator.of(context).pushReplacement(
+//          MaterialPageRoute(builder: (context) => const MainStack()),
+//        );
+//      });
+//    });
+//  }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Use post frame callback to ensure safe navigation
+//    WidgetsBinding.instance.addPostFrameCallback((_) {
+//      Future.delayed(const Duration(seconds: 2), () {
+//        Navigator.of(context).pushReplacement(
+//          MaterialPageRoute(builder: (context) => const MainStack()),
+//        );
+//      });
+//    });
+   
+//     final focusState = Provider.of<FocusStateNotifier>(context);
+//     _navigateToOnBoarding(context);
+
+//     return GestureDetector(
+//       onTap: () {
+//         focusState.unfocus(); // Unfocus the field when tapping outside
+//       },
+//       child: Scaffold(
+//         backgroundColor: Theme.of(context).splashColor,
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               // App Logo Image
+//               Container(
+//                 height: 230,
+//                 width: 230,
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(60),
+//                 ),
+//                 child: ClipOval(
+//                   child: SvgPicture.asset(
+//                     AppImages.splash,
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
-  void _navigateToOnBoarding(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const MainStack()),
-      );
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 2), () {
+        _navigateToOnBoarding();
+      });
     });
+  }
+
+  void _navigateToOnBoarding() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const MainStack()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final focusState = Provider.of<FocusStateNotifier>(context);
-    _navigateToOnBoarding(context);
 
     return GestureDetector(
       onTap: () {
-        focusState.unfocus(); // Unfocus the field when tapping outside
+        focusState.unfocus();
       },
       child: Scaffold(
         backgroundColor: Theme.of(context).splashColor,
@@ -31,7 +104,6 @@ class SplashScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // App Logo Image
               Container(
                 height: 230,
                 width: 230,
