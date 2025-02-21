da7c64d757f8958d19a66b447c71282d25751576
Fahad TT <ttfahad2317@gmail.com>
2025-02-21 09:16:49 +0400

    doc: CustomTimeagoMessages class moved to extras folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:alletre_app/app.dart';
 import 'package:flutter/material.dart';
-import 'utils/custom_timeago_messages.dart';
+import 'utils/extras/custom_timeago_messages.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 void main() {
