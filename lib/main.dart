c471d64a3abee8948913bf600d3b405f6ae3ceef
Fahad TT <ttfahad2317@gmail.com>
2025-02-24 12:27:17 +0400

    feat: firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:alletre_app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'utils/extras/custom_timeago_messages.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   timeago.setLocaleMessages('en_custom', CustomTimeagoMessages());
   runApp(const MyApp());
 }
